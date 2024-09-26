0fd75bce3df398e0cc649ba6aa6dea1afe9d2f88
Dean Logan <deanlogan42@gmail.com>
2024-09-26 09:26:24 +0100

    Added a user details manager to jdbc auth demo

--- 05-spring-boot-security/01-JDBC-auth/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-security/01-JDBC-auth/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -15,7 +15,19 @@ import javax.sql.DataSource;
 public class DemoSecurityConfig {
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // define query to retreive a user by usernmae
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?"
+        );
+
+        // define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?"
+        );
+
+        return jdbcUserDetailsManager;
     }
 
     @Bean
